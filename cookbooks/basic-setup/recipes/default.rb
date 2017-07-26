57cdc4034788fd0d943664e7228cbf4ead9d7938
Chris MacNaughton <chris@centaurisolutions.nl>
2017-07-26 14:25:36 +0200

    update git checkout

--- cookbooks/basic-setup/recipes/default.rb
+++ cookbooks/basic-setup/recipes/default.rb
@@ -61,6 +61,9 @@ projects.each do |user, project|
     git "/home/#{user_name}/projects/#{project}" do
         user user_name
         group user_name
+        checkout_branch 'master'
+        enable_checkout false
+        enable_submodules true
         repository "git@github.com:#{user}/#{project}.git"
         ssh_wrapper "/home/#{user_name}/bin/git_wrapper.sh"
         action :sync
