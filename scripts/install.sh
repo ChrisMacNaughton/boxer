35c97b5eb549703999e732987d6448ae14b109f5
Chris MacNaughton <chris@centaurisolutions.nl>
2017-07-26 13:40:49 +0200

    remove existing setup

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,5 +3,6 @@
 echo "Installing setup dependencies"
 sudo apt-get install -yqq git curl
 
+rm -rf /tmp/boxer
 git clone https://github.com/ChrisMacNaughton/boxer /tmp/boxer
 sudo /tmp/boxer/scripts/bootstrap.sh
