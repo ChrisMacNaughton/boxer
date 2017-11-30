c7382a1284fdf6d7183a32fae38c6c187c214537
Chris MacNaughton <chris@centaurisolutions.nl>
2017-11-30 16:19:21 +0100

    add irccloud dep

--- scripts/irccloud.sh
+++ scripts/irccloud.sh
@@ -3,6 +3,7 @@
 install (){
     log "Ensuring IrcCloud is installed"
     command -v IRCCloud >/dev/null 2>&1 || {
+        deps
         fetch
         deb_install /tmp/irccloud.deb
     }
@@ -12,4 +13,7 @@ fetch() {
     wget -O /tmp/irccloud.deb https://github.com/irccloud/irccloud-desktop/releases/download/v0.4.0/irccloud-desktop_0.4.0_amd64.deb
 }
 
+deps() {
+    apt_install libappindicator1
+}
 install
