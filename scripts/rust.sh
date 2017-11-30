9c57275c11e826d3c44462eeca939fa8c4405da1
Chris MacNaughton <chris@centaurisolutions.nl>
2017-11-30 16:28:43 +0100

    ensure we have all toolchains

--- scripts/rust.sh
+++ scripts/rust.sh
@@ -4,6 +4,7 @@ install (){
     log "Ensuring Rust / rustup is installed"
     command -v rustup >/dev/null 2>&1 || {
         curl https://sh.rustup.rs -sSf | sh -s -- -y --no-modify-path
+        $HOME/.cargo/bin/rustup install beta nightly
     }
 }
 
