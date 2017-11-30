ba3934803719700fdf8b8454f42e938bbd67e37f
Chris MacNaughton <chris@centaurisolutions.nl>
2017-11-30 16:24:35 +0100

    reduce root usage

--- scripts/vscode.sh
+++ scripts/vscode.sh
@@ -25,9 +25,9 @@ install_plugins() {
 
 add_repo() {
     curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-    mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-    sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-    apt-get update
+    sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+    sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+    sudo apt-get update
 }
 
 install
